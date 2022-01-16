1c263a488f126dd0f6fb6c2463c4e4c19df2c0c1
Agnidipto Sinha <agnidipto12.25@gmail.com>
2022-01-16 21:53:30 +0530

    Workers

--- init2.bat
+++ init2.bat
@@ -6,6 +6,6 @@ pip install -r requirements.txt
 ECHO ===============================
 ECHO Installed. Launching server on port 8080
 ECHO ===============================
-python main.py
+uvicorn main:app --reload
 START http://localhost:8080/docs
 PAUSE
